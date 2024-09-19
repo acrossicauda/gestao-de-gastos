3a52138a81e8ee819293055591281a68ae0387a8
Tiago Sousa <acrossicauda@hotmail.com.br>
2024-09-19 17:14:47 -0300

    ...

---
+++ app/frontend/src/pages/Calendar.vue
@@ -0,0 +1,312 @@
+<template>
+  <!-- <FlexboxLayout flexWrap="wrap">
+    <div class="d-none">
+      <CalendarComp2 />
+    </div>
+    <div>
+      <DayPilotMonth id="dp" :config="config" ref="monthRef" />
+    </div>
+  </FlexboxLayout> -->
+  <DayPilotMonth id="dp" :config="config" ref="monthRef" />
+</template>
+
+<script setup>
+import {DayPilot, DayPilotMonth} from '@daypilot/daypilot-lite-vue';
+import { ref, reactive, onMounted } from 'vue';
+import CalendarComp2 from '../components/CalendarComp2.vue';
+
+
+
+function getDateNow() {
+  let dateToString = d => `${d.getFullYear()}-${('00' + (d.getMonth() + 1)).slice(-2)}-${('00' + d.getDate()).slice(-2)}` 
+  let date = new Date();
+
+  return dateToString(date);
+}
+
+
+async function getEvents() {
+
+  let events = [
+      {
+          id: 1,
+          start: getDateNow()+"T00:00:00", //"2021-09-10T10:00:00",
+          end: getDateNow()+"T00:00:00", //"2021-09-10T10:00:00",
+          text: "Event Teste",
+          backColor: "#6aa84f",
+          borderColor: "#38761d",
+      },
+      {
+          id: 2,
+          start: "2023-02-28T13:00:00",
+          end: "2025-02-28T16:00:00",
+          text: "Event 2",
+          // backColor: "#f1c232",
+          // borderColor: "#bf9000",
+      },
+  ];
+
+  
+  return events;
+}
+
+
+async function getCalendar() {
+  let resp = [];
+  const options = {
+    method: 'GET',
+    headers: {
+      cookie: 'XSRF-TOKEN=eyJpdiI6InhCeXl6Z0QxcDFuUmhUd0tVR01TREE9PSIsInZhbHVlIjoiRlVQUGY2WHJybUIwVkhMVXhMN3Z1M3NTTjc1clRPY0FPZzc4YmFLa2hPbGxTY2ZYbTE2U2Fja0JmYitzM3RLc2ozMDJBVWl1K2l5VVllcGhaYS9nVlJNVGdvdlZtSXA3N0RoUVhFY3VEMm9hVUdoQlkyZi9MeCtJM3ZtanlWUnAiLCJtYWMiOiJlMDUyZjk3NzFmOGRkNWIyMGI4MDRlMWIxZjBhMjgyZWQ3NzI3ZTE2OTI5YmQ4OTMyMGRjYzNmMjM2MTdmZTM4IiwidGFnIjoiIn0%253D; laravel_session=eyJpdiI6IktNcUV4RFlyVWsreTNLQ2tzWlRkZ2c9PSIsInZhbHVlIjoiaW1vVW1PNjRKbnV0amNRc1hKN0R1SzNVQXZOb1UvSmc2amF6cWVsQ1lXQnZ2TzUzU2ptWVgycExCdEZrM2hnUDFRYTRraUJOTXEvNTZ0My9iTmFWc0s2VXRyYkI1UnVRQmFoUld0YzNkYmM1aWk4SlZ1bEJqOWpwUGFodnhscFUiLCJtYWMiOiIxMDg2NWZjYzVjNjhjNzE3MjFiMzM0YjljOWQ3M2ZlOTRlY2I5NDc1NzBkMmM0MDE3YjlmZWQ5OTNiMDAzZjY5IiwidGFnIjoiIn0%253D',
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer 6|qrFg34xSbYsVhh4fN67NRVrdIPPbK91saEalYH8le8015bd7'
+    }
+  };
+
+  await fetch('http://localhost:8000/api/calendar', options)
+    .then(response => resp = response.json() )
+    // .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    return resp;
+}
+
+
+
+const config = reactive({
+  startDate: getDateNow(),
+  viewType: "Week",
+  durationBarVisible: true,
+  eventHeight: 30,
+  onEventMoved: (args) => {
+    const name = args.e.data.text;
+    console.log(`Event ${name} moved to ${args.newStart}`);
+  },
+  onEventResized: (args) => {
+    console.log("Event resized: " + args.e.text());
+  },
+  onEventClicked: async (args) => {
+    const form = [
+      {name: "Text", id: "text"}
+    ];
+    const modal = await DayPilot.Modal.form(form, args.e.data);
+    if (modal.canceled) {
+      return;
+    }
+    console.log('TESTEEEE');
+    console.log(modal.result);
+    monthRef.value.control.events.update(modal.result);
+  },
+  onTimeRangeSelected: async args => {
+    const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
+    const dp = args.control;
+    dp.clearSelection();
+    if (modal.canceled) { return; }
+    dp.events.add({
+      start: args.start,
+      end: args.end,
+      id: DayPilot.guid(),
+      text: modal.result
+    });
+
+    console.log(args)
+    console.log(dp.events)
+  },
+  onBeforeEventRender: args => {
+    const color = colors[args.data.type] || "#3c78d8";
+    args.data.backColor = color + "55";
+    args.data.borderColor = color + "44";
+    args.data.barColor = color + "66";
+
+    args.data.areas = [
+      {
+        right: 4,
+        top: 3,
+        width: 24,
+        height: 24,
+        padding: 2,
+        fontColor: "#444444",
+        backColor: color + "55",
+        symbol: "icons/daypilot.svg#threedots-v",
+        action: "ContextMenu",
+        style: "border-radius: 50%"
+      }
+    ];
+
+    if (args.data.locked) {
+      args.data.text += " (locked)";
+      args.data.areas.push({
+        right: 30,
+        top: 3,
+        width: 24,
+        height: 24,
+        padding: 4,
+        fontColor: "#444444",
+        backColor: color + "55",
+        symbol: "icons/daypilot.svg#padlock",
+        style: "border-radius: 50%",
+        onClick: async args => {
+          const modal = await DayPilot.Modal.confirm("Do you really want to unlock this event?");
+          if (modal.canceled) {
+            return;
+          }
+          toggleEventLock(args.source);
+        }
+      });
+
+      args.data.moveDisabled = true;
+      args.data.resizeDisabled = true;
+      args.data.clickDisabled = true;
+      args.data.deleteDisabled = true;
+    }
+  },
+  contextMenu: new DayPilot.Menu({
+    items: [
+      {text: "Edit...", onClick: args => editEvent(args.source) },
+      {text: "Delete", symbol: "icons/daypilot.svg#x-4", onClick: args => deleteEvent(args.source) },
+      {text: "Lock", symbol: "icons/daypilot.svg#padlock", onClick: args => toggleEventLock(args.source) },
+      {text: "-"},
+      {text: "Duplicate", onClick: args => duplicateEvent(args.source) },
+      {text: "Postpone", symbol: "icons/daypilot.svg#minichevron-right-4", onClick: args => postponeEvent(args.source) },
+      {text: "-"},
+      {text: "Type", items: [
+          {text: "Event", icon: "icon icon-blue", onClick: args => updateEventType(args.source, "event")},
+          {text: "Task", icon: "icon icon-green", onClick: args => updateEventType(args.source, "task")},
+          {text: "Reminder", icon: "icon icon-yellow", onClick: args => updateEventType(args.source, "reminder")},
+          {text: "Holiday", icon: "icon icon-red", onClick: args => updateEventType(args.source, "holiday")},
+        ]
+      }
+    ],
+    onShow: args => {
+      const e = args.source;
+      const locked = e.data.locked;
+
+      // update the lock/unlock text
+      args.menu.items[2].text = locked ? "Unlock" : "Lock";
+
+      // disable actions for locked
+      args.menu.items[0].disabled = locked;
+      args.menu.items[1].disabled = locked;
+      args.menu.items[5].disabled = locked;
+      args.menu.items[7].disabled = locked;
+    }
+  })
+});
+const monthRef = ref(null);
+
+const colors = {
+  "event": "#3c78d8",
+  "task": "#6aa84f",
+  "reminder": "#f1c232",
+  "holiday": "#cc0000",
+};
+
+const editEvent = async (e) => {
+  const form = [
+    {name: "Text", id: "text"}
+  ];
+
+  const modal = await DayPilot.Modal.form(form, e.data);
+  if (modal.canceled) {
+    return;
+  }
+
+  monthRef.value.control.events.update(modal.result);
+};
+
+const deleteEvent = async (e) => {
+  const modal = await DayPilot.Modal.confirm("Do you really want to delete this event?");
+  if (modal.canceled) {
+    return;
+  }
+  monthRef.value.control.events.remove(e);
+};
+
+const duplicateEvent = (e) => {
+  const newEvent = {
+    ...e.data,
+    id: DayPilot.guid()
+  };
+  monthRef.value.control.events.add(newEvent);
+};
+
+const postponeEvent = (e) => {
+  e.data.start = e.start().addDays(1);
+  e.data.end = e.end().addDays(1);
+  monthRef.value.control.events.update(e);
+};
+
+const updateEventType = (e, type) => {
+  e.data.type = type;
+  monthRef.value.control.events.update(e);
+}
+
+const toggleEventLock = (e) => {
+  e.data.locked = !e.data.locked;
+  monthRef.value.control.events.update(e);
+};
+
+
+const loadEvents = async () => {
+  let events;
+  let response = await getCalendar();
+
+  if(!response.success) {
+    throw "ERRO";
+  }
+  events = response.data;
+
+  config.events = events;
+
+  // console.log(monthRef.value.control.events)
+  //const modal = await DayPilot.Modal.form(form, args.e.data);
+  events.forEach(event => {
+    monthRef.value.control.events.add(event);
+  })
+
+};
+
+onMounted(() => {
+  loadEvents();
+});
+
+
+</script>
+
+
+<style>
+
+.month_default_event_inner {
+  border-radius: 20px;
+  padding-left: 35px;
+
+  white-space: nowrap;
+}
+
+.month_default_event_bar_inner {
+  width: 30px;
+  border-radius: 20px;
+}
+
+.month_default_event svg:hover {
+  color: #000000;
+  cursor: pointer;
+}
+
+/* context menu icons */
+.icon:before {
+  position: absolute;
+  left: 0px;
+  margin-left: 8px;
+  margin-top: 3px;
+  width: 14px;
+  height: 14px;
+  content: '';
+}
+
+.icon-blue:before { background-color: #3c78d8; }
+.icon-green:before { background-color: #6aa84f; }
+.icon-yellow:before { background-color: #f1c232; }
+.icon-red:before { background-color: #cc0000; }
+
+</style>
